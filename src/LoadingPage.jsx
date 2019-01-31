39ab530868722042b473dd6854fb43b2d1fbd339
Aaron Gottlieb <atticus19@gmail.com>
2019-01-30 18:01:09 -0700

    sd

--- src/LoadingPage.jsx
+++ src/LoadingPage.jsx
@@ -5,9 +5,7 @@ const LoadingPage = () => (
   <div className="App">
     <h1>Your Movies are Loading</h1>
     <img src={logo} className="App-logo" alt="logo" />
-    <h2>
-      <span>hello</span>
-    </h2>
+    <h2>Yay</h2>
   </div>
 );
 
